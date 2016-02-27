ccacbddfbb8bf75fb08e82916d12071eb8708f4a
Md Shahbaz Alam <shahbazalam17feb@gmail.com>
2016-02-27 12:22:54 +0530

    added ui

---
+++ app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+ 	$('select').material_select();
+ 	
+    $(".button-collapse").sideNav();
+        
+    $('.collapsible').collapsible({
+      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
+});
+
+
